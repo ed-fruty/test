fc97f2a9de49914fc57896ca947902a92f4c5e0d
Eduard <ed.fruty@gmail.com>
2014-12-01 00:45:09 +0200

    Move errors settings to the service provider

---
+++ app/Support/ServiceProviders/ErrorsServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Support\ServiceProviders;
+
+/**
+ * Class ErrorsServiceProvider
+ * @package App\Support\ServiceProviders
+ * @category ErrorsServiceProvider
+ * @author Fruty <ed.fruty@gmail.com>
+ */
+class ErrorsServiceProvider
+{
+
+} 
